255700b8f8e7208c2b9d865e9a77216e28348c7f
Sid Wood <sidwood@me.com>
2016-04-13 08:15:01 +0100

    Update configs and rules to eslint v2.3.0

---
+++ configs/core.js
@@ -0,0 +1,23 @@
+'use strict';
+
+module.exports = {
+  'env': {
+    es6: true
+  },
+  'extends': [
+    'sidwood/rules/best-practices',
+    'sidwood/rules/ecmascript-6',
+    'sidwood/rules/nodejs-commonjs',
+    'sidwood/rules/possible-errors',
+    'sidwood/rules/strict-mode',
+    'sidwood/rules/stylistic-issues',
+    'sidwood/rules/variables'
+  ],
+  'globals': {
+  },
+  'parserOptions': {
+    ecmaVersion: 6
+  },
+  'rules': {
+  }
+};
--- configs/node.js
+++ configs/node.js
@@ -1,41 +1,16 @@
 'use strict';
 
 module.exports = {
-  'extends': [
-    'eslint-config-sidwood/rules/best-practices.js',
-    'eslint-config-sidwood/rules/ecmascript-6.js',
-    'eslint-config-sidwood/rules/nodejs-commonjs.js',
-    'eslint-config-sidwood/rules/possible-errors.js',
-    'eslint-config-sidwood/rules/strict-mode.js',
-    'eslint-config-sidwood/rules/stylistic-issues.js',
-    'eslint-config-sidwood/rules/variables.js'
-  ],
   'env': {
-    browser: false,
-    node: true,
-    es6: false
+    node: true
   },
-  'ecmaFeatures': {
-    arrowFunctions: true,
-    binaryLiterals: true,
-    blockBindings: true,
-    classes: true,
-    defaultParams: false,
-    destructuring: true,
-    forOf: true,
-    generators: true,
-    modules: false,
-    objectLiteralComputedProperties: true,
-    objectLiteralDuplicateProperties: false,
-    objectLiteralShorthandMethods: true,
-    objectLiteralShorthandProperties: true,
-    regexYFlag: true,
-    regexUFlag: true,
-    spread: true,
-    superInFunctions: true,
-    templateStrings: true,
-    jsx: false
+  'extends': [
+    'sidwood/configs/core'
+  ],
+  'globals': {
   },
-  'globals': {},
-  'rules': {}
+  'parserOptions': {
+  },
+  'rules': {
+  }
 };
